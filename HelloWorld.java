761c0bb5a2e63c6066ee73fd54a1d10a76e35266
Vinay Kumar <121301076+vinayk090@users.noreply.github.com>
2024-01-10 15:58:46 +0530

    HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld
 {
     public static void main(String arg[])
         {
-           System.out.println("Hello World");
+           System.out.println("Hello Welcome to the jenkins World");
        }
 }
